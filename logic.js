b28e036939798d4659690da9e85f6532da3979fc
levymikael <levymikael@gmail.com>
2019-06-27 21:07:43 +0300

    missing 1 last thing

--- logic.js
+++ logic.js
@@ -3,16 +3,18 @@ class App extends React.Component {
         super(props);
         this.state = {
             toDoItem: [],
-            done: []
+            done: [],
+            display : "block"
         };
         this.fromToDoToDone = this.fromToDoToDone.bind(this);
         this.removeItem = this.removeItem.bind(this);
+        this.removeItem2 = this.removeItem2.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
     removeItem(str) {
         var index = 0;
-        for (var i = 0 ; i < this.state.toDoItem.length ; i++){
-            if (this.state.toDoItem[i] === str ){
+        for (var i = 0; i < this.state.toDoItem.length; i++) {
+            if (this.state.toDoItem[i] === str) {
                 index = i
             }
         }
@@ -21,13 +23,27 @@ class App extends React.Component {
             toDoItem: this.state.toDoItem
         })
     }
-    fromToDoToDone(item) {
-        this.state.done.push(item);
-        console.log(this.state.done);
+    removeItem2(str) {
+        var index = 0;
+        for (var i = 0; i < this.state.done.length; i++) {
+            if (this.state.done[i] === str) {
+                index = i
+            }
+        }
+        this.state.done.splice(index, 1);
         this.setState({
             done: this.state.done
+        })
+    }
+    fromToDoToDone(item) {
+        this.state.done.push(item);
+        // this.state.toDoItem.
+        console.log(this.state.done);
+        this.setState({
+            done: this.state.done,
+            display:"none"
         });
-        
+
     }
     handleSubmit() {
         this.state.toDoItem.push(this.name.value)
@@ -40,19 +56,19 @@ class App extends React.Component {
         return (
             <div>
                 <div id="header">
-                    <h4>To Do list</h4>
+                    <h4>To Do list</h4><img src="https://img.icons8.com/bubbles/100/000000/list.png" />
                 </div>
                 <div id="list">
                     <input id="inputField" ref={input => this.name = input} placeholder="You gotta do what you gotta do"></input>
-                    <button id="inputBtn" onClick={this.handleSubmit}>ADD</button>
-                </div>
-                <div className="toDoList">
-                    <h4>To Do</h4>
-                    <List name="todo" removeItem={this.removeItem} design="todo-object" items={this.state.toDoItem} callingFromToDoToDone={this.fromToDoToDone}></List>
-                </div>
-                <div className="doneList">
-                    <h4>Done</h4>
-                    <List name="done" removeItem={this.removeItem} design="done-object" items={this.state.done}></List>
+                    <button id="inputBtn" onClick={this.handleSubmit}>Add</button>
+                    <div className="toDoList">
+                        <h4>To Do</h4>
+                        <List removeItem={this.removeItem} style={{display :this.state.display}} design="todo-object" items={this.state.toDoItem} position={this.props.newKey} callingFromToDoToDone={this.fromToDoToDone}></List>
+                    </div>
+                    <div className="doneList">
+                        <h4>Done</h4>
+                        <List removeItem={this.removeItem2} design="done-object" items={this.state.done}></List>
+                    </div>
                 </div>
             </div>
         )
@@ -67,15 +83,25 @@ class List extends React.Component {
     callingFromToDoToDone(item) {
         this.props.callingFromToDoToDone(this.props.item)
         console.log("yaytwo")
-
     }
     render() {
+        var test = this.props.items.map((item, index) => {
+            return (
+                <Item
+                    removeItem={this.props.removeItem}
+                    removeItem2={this.props.removeItem}
+                    newKey={index}
+                    className={this.props.design}
+                    item={item}
+                    callingFromToDoToDone={this.props.callingFromToDoToDone}
+                    style = {this.props.style}>
+
+                </Item>
+            );
+        });
         return (
             <ol>
-                <li>  {this.props.name}</li>
-                {
-                    this.props.items.map((item) => <Item removeItem={this.props.removeItem} className={this.props.design} item={item} callingFromToDoToDone={this.props.callingFromToDoToDone}></Item>)
-                }
+                {test}
             </ol>
         )
     }
@@ -86,20 +112,27 @@ class Item extends React.Component {
         super(props);
         this.callingFromToDoToDone = this.callingFromToDoToDone.bind(this);
         this.removeItem = this.removeItem.bind(this);
+        this.removeItem2 = this.removeItem2.bind(this);
+        this.state={
+            hide: false
+        }
     }
     removeItem() {
         this.props.removeItem(this.props.item)
     }
+    removeItem2() {
+        this.props.removeItem2(this.props.item)
+    }
     callingFromToDoToDone() {
         this.props.callingFromToDoToDone(this.props.item);
-        console.log("yayone")
-
     }
     render() {
+        
         return (
-            <div className="itemStyle">
+            <div className="itemStyle" style={this.props.style}>
                 <li onClick={this.callingFromToDoToDone} className={this.props.design}>{this.props.item}</li>
-                <button onClick={this.removeItem}>Remove</button>
+                <img id="removeBtn" onClick={this.removeItem}src="https://img.icons8.com/clouds/100/000000/add-trash.png"/>
+             
             </div>)
     }
 }
